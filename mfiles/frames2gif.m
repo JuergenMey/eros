2bb99e70aedb8a195d3c1a2a4b654b0dea4f546d
Dr. rer. nat. Jürgen Mey <juergen.mey@uni-potsdam.de>
2024-03-04 10:24:04 +0100

    frames --> F

--- mfiles/frames2gif.m
+++ mfiles/frames2gif.m
@@ -1,7 +1,7 @@
-function frames2gif(frames,filename,delay) 
+function frames2gif(F,filename,delay) 
 
-for idx = 1:length(frames)
-    [A,map] = rgb2ind(frame2im(frames(idx)),256);
+for idx = 1:length(F)
+    [A,map] = rgb2ind(frame2im(F(idx)),256);
     if idx == 1
         imwrite(A,map,filename,'gif','LoopCount',Inf,'DelayTime',delay);
     else
